727721e3aaf6988af3336a730bf83d53bc4f1c35
mpere <mpereannor@gmail.com>
2024-03-27 13:59:00 +0000

    feat: create templates for error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -30,8 +30,8 @@ def create_app(config_class=Config):
     moment.init_app(app)
     babel.init_app(app)
 
-    # from app.errors import bp as errors_bp
-    # app.register_blueprint(errors_bp)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
