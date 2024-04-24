913ebbfbabc5a4dfa64eabc1f27f94c161395bc5
mpere <mpereannor@gmail.com>
2024-04-24 14:41:36 +0000

    feat: elasticsearch config

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from config import Config
-
+from elasticsearch import Elasticsearch
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
@@ -22,6 +22,8 @@ babel = Babel()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
 
     db.init_app(app)
     migrate.init_app(app, db)
--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mpereannor@gmail.com']
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     # LANGUAGES = ['en', 'es']
     # MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     # POSTS_PER_PAGE = 25
