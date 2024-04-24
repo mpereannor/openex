d03ba4d77d6da8712cf3ea2d34098aa2de165b15
mpere <mpereannor@gmail.com>
2024-04-24 16:12:01 +0000

    feat: integrate searches with sqlalchemy

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
+from app.search import add_to_index, remove_from_index, query_index
 
 Base = declarative_base()
 class User(UserMixin, db.Model):
@@ -73,8 +74,10 @@ class Account(db.Model):
     def __repr__(self):
         return f'<Account #{self.name}>'
 
-class Transaction(db.Model):
+class Transaction(SearchableMixin, db.Model):
     __tablename__ = 'transactions'
+    #model needs to have its body field indexed for searching
+    __searchable__ = ['body']
     id: so.Mapped[UUID] = so.mapped_column(default=lambda: str(uuid4()), index=True, primary_key=True)
     transaction_id: so.Mapped[int] = so.mapped_column(sa.Integer, index=True, autoincrement=True)
     created_at: so.Mapped[Optional[datetime]] = so.mapped_column(
@@ -94,3 +97,45 @@ class Transaction(db.Model):
 
 
 
+class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total = 0:
+            return [], 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        query = sa.select(cls).where(cls.id.in_(ids)).order_by(
+            db.case(*when, value=cls.id)
+        )
+        return db.session.scalars(query), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = { 
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None 
+
+    @classmethod
+    def reindex(cls):
+        for obj in db.session.scalars(sa.select(cls)):
+            add_to_index(cls.__tablename__, obj)
+    
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
